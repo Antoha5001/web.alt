05051ce3ec9cd0745d3012192436c4adedf8fb5e
Antoha5001 <plantsvzzombi@mail.ru>
2016-09-12 16:10:43 +0700

    Remove wow to offer

---
+++ script/paralax.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){	 
+
+	setTimeout(function(){
+		$('.header__logo').addClass('is-visible');
+		$('.header__phone').addClass('is-visible');
+	},1000);
+	
+	setTimeout(function(){
+		$('.header__zagolovok').addClass('is-visible');
+		$('.offer__spisok-block').addClass('is-visible');
+	},1500);
+	
+	setTimeout(function(){
+		$('.offer__img').addClass('is-visible');
+		$('.hr').addClass('is-visible');
+	},2000);
+	
+	setTimeout(function(){
+		$('.form-box').addClass('is-visible');
+		$('.offer__strelka').addClass('is-visible');
+	},2500);
+	
+});
