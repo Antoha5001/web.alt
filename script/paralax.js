980da9fa80cf754444dbf3024f2ce33b08b5ad0c
Antoha5001 <plantsvzzombi@mail.ru>
2016-09-13 18:26:33 +0700

    footer align center

--- script/paralax.js
+++ script/paralax.js
@@ -28,21 +28,31 @@ $(document).ready(function(){
 		toolsBlock = $('.tools-box'),
 		toolsItem = toolsBlock.find('.base-block__item'),
 		browserBox = $('.browser-box'),
-		browserItem = browserBox.find('.base-block__item');
+		browserItem = browserBox.find('.base-block__item'),
+		section = $('section'),
+			timer=500;
 		
-		if(wScroll>toolsBlock.offset().top - ($(window).height()/1.8)){
+		if(wScroll>toolsBlock.offset().top - ($(window).height()/1.6)){
 			toolsItem.each(function(e){
 				setTimeout(function(){	
 					toolsItem.eq(e).addClass('is-visible');
-				},300 * (e+1));
+				},timer * (e+1));
 			});
 		}
-		if(wScroll>browserBox.offset().top - ($(window).height()/1.8)){
+		if(wScroll>browserBox.offset().top - ($(window).height()/1.6)){
 			browserItem.each(function(e){
 				setTimeout(function(){	
 					browserItem.eq(e).addClass('is-visible');
-				},500 * (e+1));
+				},timer * (e+1));
 			});
 		}
+		/*if(wScroll>section.offset().top - ($(window).height()/1.6)){
+			section.each(function(e){
+				setTimeout(function(){	
+				var 
+		zagolovok = e.find('.zagolovok');	zagolovok.eq(e).addClass('is-visible');
+				},timer * (e+1));
+			});
+		}*/
 	});
 });
